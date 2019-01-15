98e8e5cfae18d0a7e28f2fdb364d4a885262b071
phillip <windowsyyf@gmail.com>
2019-01-14 22:01:21 -0500

    test

---
+++ my-app/src/test/java/com/mycompany/app/ArrayTest.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ArrayTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
